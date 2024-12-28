23f3e8474dc8b7939b3a35d76e2bcce9fee29dc9
Johannes Maier <johannes.maier@mailbox.org>
2024-12-28 07:47:12 +0100

    implement simple main loop

--- src-lib/Shmup/Main.hs
+++ src-lib/Shmup/Main.hs
@@ -7,6 +7,24 @@ drawPrompt = do
     putStr "$ "
     hFlush stdout
 
+data Builtin
+    = Exit
+    | Cd String
+    deriving (Show, Eq)
+
+data Command
+    = Builtin Builtin
+    | Other String
+    deriving (Show, Eq)
+
+readCommand :: String -> Command
+readCommand line =
+    case words line of
+        [] -> error "impossible?"
+        "exit" : _ -> Builtin Exit
+        "cd" : dir : _ -> Builtin (Cd dir)
+        _ -> Other line
+
 main :: IO ()
 main = do
     hSetBuffering stdout LineBuffering
@@ -14,8 +32,14 @@ main = do
   where
     go = do
         drawPrompt
-        cmd <- getLine
-        case cmd of
-            "exit" -> pure ()
-            _ -> do
+        line <- getLine
+        case readCommand line of
+            Builtin Exit -> do
+                putStrLn "Exiting..."
+                pure ()
+            Builtin (Cd dir) -> do
+                putStrLn $ "Changing directory to: " <> dir
+                go
+            Other cmd -> do
+                putStrLn $ "The command was: " <> cmd
                 go
