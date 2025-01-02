f7d0920556d3ca62a1d15136fe5f4e4d21c9bfcf
Johannes Maier <johannes.maier@mailbox.org>
2025-01-02 13:06:59 +0100

    use state to incrementally read input
    This sucks and is probably still quite wrong, but a step towards pure
    state handling maybe?

--- src-lib/Shmup/Main.hs
+++ src-lib/Shmup/Main.hs
@@ -1,15 +1,18 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE NoFieldSelectors #-}
+
 module Shmup.Main (main) where
 
+import Control.Monad (void)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.State.Class (MonadState)
+import Control.Monad.State.Strict qualified as State
 import Data.List.NonEmpty (NonEmpty)
 import Data.List.NonEmpty qualified as NonEmpty
 import System.Directory qualified as Dir
 import System.IO (BufferMode (..), hFlush, hSetBuffering, stdout)
 import System.Process qualified as Proc
-
-drawPrompt :: IO ()
-drawPrompt = do
-    putStr "$ "
-    hFlush stdout
 
 data Command
     = None
@@ -20,8 +23,8 @@ data Command
     | Other (NonEmpty String)
     deriving stock (Show, Eq)
 
-readCommand :: String -> Command
-readCommand line =
+parseCommand :: String -> Command
+parseCommand line =
     case words line of
         [] -> None
         "exit" : _ -> Exit
@@ -30,28 +33,76 @@ readCommand line =
         "echo" : msg -> (Echo $ unwords msg)
         (x : xs) -> Other (x NonEmpty.:| xs)
 
+data S = S
+    { input :: String
+    }
+    deriving stock (Show)
+
+class (Monad m) => TTY m where
+    flush :: m ()
+    getch :: m Char
+    putch :: Char -> m ()
+    write :: String -> m ()
+
+instance (Monad m, MonadIO m) => TTY m where
+    flush = liftIO $ hFlush stdout
+    getch = liftIO getChar
+    putch = liftIO . putChar
+    write = liftIO . putStr
+
+writeLn :: (TTY m) => String -> m ()
+writeLn s = do
+    write s
+    putch '\n'
+    flush
+
+drawPrompt :: (TTY m) => m ()
+drawPrompt = write "$ " >> flush
+
+data Input
+    = Unfinished String
+    | Command Command
+    deriving stock (Show)
+
+readInput :: (Shell m) => m Command
+readInput = do
+    c <- getch
+    case c of
+        '\n' -> do
+            input <- State.gets (.input)
+            State.modify' (\s -> s{input = mempty})
+            pure $ parseCommand input
+        _ -> do
+            State.modify' (\s -> s{input = s.input ++ [c]})
+            readInput
+
+type Shell m = (TTY m, MonadState S m)
+
+run :: (Shell m, MonadIO m) => m ()
+run = do
+    drawPrompt
+    command <- readInput
+    case command of
+        None -> run
+        Exit -> do
+            writeLn "Exiting..."
+        CD dir -> do
+            writeLn $ "Changing directory to: " <> dir
+            liftIO $ Dir.setCurrentDirectory dir
+            run
+        PWD -> do
+            pwd <- liftIO Dir.getCurrentDirectory
+            writeLn $ "Current directory: " <> pwd
+            run
+        Echo msg -> do
+            writeLn msg
+            run
+        Other (cmd NonEmpty.:| args) -> do
+            writeLn $ "The command was: " <> cmd <> ", " <> show args
+            liftIO $ Proc.callProcess cmd args
+            run
+
 main :: IO ()
 main = do
     hSetBuffering stdout LineBuffering
-    go
-  where
-    go = do
-        drawPrompt
-        line <- getLine
-        case readCommand line of
-            None -> go
-            Exit -> do
-                putStrLn "Exiting..."
-            CD dir -> do
-                putStrLn $ "Changing directory to: " <> dir
-                Dir.setCurrentDirectory dir
-                go
-            PWD -> do
-                pwd <- Dir.getCurrentDirectory
-                putStrLn $ "Current directory: " <> pwd
-                go
-            Echo msg -> putStrLn msg *> go
-            Other (cmd NonEmpty.:| args) -> do
-                putStrLn $ "The command was: " <> cmd <> ", " <> show args
-                Proc.callProcess cmd args
-                go
+    void $ State.runStateT run (S mempty)
